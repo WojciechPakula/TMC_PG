9bec3629d82029b35c82117c6f4068fa219d1290
WojciechPakula <wojciechpaku@gmail.com>
2018-04-11 11:24:56 +0300

    Test skalowania

--- Assets/BitMapTest.cs
+++ Assets/BitMapTest.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BitMapTest : MonoBehaviour {
+    public int x;
+    public int y;
+
+    public Vector2 wsp;
 
     SpriteRenderer rend;
-
-	// Use this for initialization
-	void Start () {
+    Texture2D tex;
+    // Use this for initialization
+    void Start () {
         rend = GetComponent<SpriteRenderer>();
 
-        Texture2D tex = new Texture2D(100,100);
+        tex = new Texture2D(x,y);
 
         for (int x = 0; x < tex.width; ++x)
         {
@@ -35,6 +39,8 @@ public class BitMapTest : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        Sprite newSprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), wsp);
+
+        rend.sprite = newSprite;
+    }
 }
