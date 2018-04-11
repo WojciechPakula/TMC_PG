3bb72540ebea8a75cf532359a705b6efc6ab8494
Ilethas <mkkurowski@gmail.com>
2018-04-11 15:22:34 +0200

    Nowy stuff w GISnode

--- Assets/Scripts/GISnode.cs
+++ Assets/Scripts/GISnode.cs
@@ -3,5 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GISnode {
-    //coś
+    public ulong id { get; set; }
+    public Vector2d latlon { get; set; }
+    public Dictionary<string, string> tags { get; set; }
+    
+    public GISnode(ulong id = 0, double latitude = 0.0, double longitude = 0.0)
+    {
+        this.id = id;
+        latlon = new Vector2d(latitude, longitude);
+        tags = new Dictionary<string, string>();
+    }
+
+    public GISnode(ulong id, Vector2d latlon, Dictionary<string, string> tags)
+        : this(id, latlon.x, latlon.y)
+    {
+        this.tags = tags;
+
+    }
 }
