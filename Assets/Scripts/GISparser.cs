d61dd8b525d0ba484038aa3b7afe21556ee4d643
WojciechPakula <wojciechpaku@gmail.com>
2018-04-11 13:20:53 +0300

    GISparser ma metody dla projekcji Pseudo-Mercatora

--- Assets/Scripts/GISparser.cs
+++ Assets/Scripts/GISparser.cs
@@ -8,4 +8,20 @@ public static class GISparser {
         //coś
         return null;
     }
+
+    //generator płaskiej ziemi
+    public static Vector2 LatlonToXY(Vector2 latlon)
+    {
+        Vector2 result;
+        result.y = (float)(System.Math.Log(System.Math.Tan((latlon.y + 90) / 360 * System.Math.PI)) / System.Math.PI * 180);
+        result.x = latlon.x;
+        return result;
+    }
+    public static Vector2 XYtoLatlon(Vector2 XY)
+    {
+        Vector2 result;       
+        result.y = (float)(System.Math.Atan(System.Math.Exp((double)XY.y / 180 * System.Math.PI)) / System.Math.PI * 360 - 90);
+        result.x = XY.x;
+        return result;
+    }
 }
